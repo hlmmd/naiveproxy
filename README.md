e7a9fa64a136930892bd6fbaf0d1ffebd95f86ff
hlmmd <hlmmd1995@gmail.com>
2019-07-25 22:32:33 +0800

    update readme and install

--- README.md
+++ README.md
@@ -10,7 +10,47 @@
 
 ## 使用方法
 
-## 原理
+安装：
+
+```bash
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+sudo touch /etc/naiveproxy.conf
+```
+
+使用：配置naiveproxy.conf文件
+
+```
+sudo vi /etc/naiveproxy.conf
+#参数说明：
+#协议类型，目前只支持TCP
+#IOTYPE: 内网到外网OUTIN或者外网到内网INOUT
+#proxy服务器IP地址
+#proxy服务器端口
+#目的IP地址
+#目的端口
+#例子：外网想通过proxy服务器(192.168.3.90)的6666号端口访问内网(192.168.100.233)的80端口（web服务）
+TCP OUTIN   192.168.3.90    6666    192.168.100.233 80
+```
+
+关闭进程：
+
+```
+killall -9 naiveproxy
+```
+
+## 技术原理
+
+基本流程：创建守护进程，读取配置文件，每一条配置fork一个子进程进行处理。
+
+每一个子进程accept对应proxy server IP和port上的连接请求，并根据其destination ip和port向目的地发出connect请求，建立两个连接。
+
+多个连接使用**epoll**IO复用模型，ET工作模式。
+
+两个连接对应两个socket，使用哈希表进行映射，当一个socket可读的时候，读取数据，然后通过查询哈希表得到对端的socket，写入读到的数据。
 
 ## TODO LIST
 
@@ -20,4 +60,5 @@
 * 零拷贝技术
 * 细节处理
 * 兼容性问题
+* 完善安装脚本
 * ……
--- install.sh
+++ install.sh
@@ -1,9 +1,68 @@
 #!/bin/bash
 
-# rm -rf * build/
-# mkdir build
-# cd build
-# cmake ../
-# make
-# sudo make install
-# cd ..
+
+# 安装naiveproxy
+
+rm -rf build/
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+cd ..
+
+# 添加到service列表中
+
+#添加service 脚本
+# echo "#!/bin/bash
+# start() 
+# {
+#     /usr/local/bin/naiveproxy
+# }
+
+# stop() 
+# {
+#     kill -9 $(pidof naiveproxy)	
+# }
+
+# case "$1" in
+# start)
+#     start
+#     ;;
+
+# stop)
+#     stop
+#     ;;
+
+# restart)
+#     stop
+#     start
+#     ;;
+
+# *)
+#     echo \"Usage: $0 {start|stop|restart}\"
+#     exit 0
+#     ;;
+
+# esac
+# exit 0" >/etc/init.d/naiveproxy1
+
+# #设置可执行权限
+# sudo chmod a+x /etc/init.d/naiveproxy1
+
+
+# [Unit]
+# Description=naiveproxy server
+# After=
+
+# [Service]
+# Type=forking
+# PIDFile=/run/naiveproxy.pid
+# #ExecStartPre=/usr/sbin/nginx -t
+# ExecStart=naiveproxy
+# ExecReload=killall -9 naiveproxy && naiveproxy
+# ExecStop=killall -9 naiveproxy
+# PrivateTmp=true
+
+# [Install]
+# WantedBy=multi-user.target
